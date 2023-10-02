327ea1ebefaeeb73eca1e703d8518a4ccc890b7f
mohamedmehdigara <garamehdi57@yahoo.com>
2023-10-02 16:55:08 +0100

    edited files

--- src/App.js
+++ src/App.js
@@ -13,76 +13,92 @@ const GameContainer = styled.div`
 const App = () => {
   const [paddleX, setPaddleX] = useState(200);
   const [ballX, setBallX] = useState(240);
-  const [ballY, setBallY] = useState(300);
+  const [ballY, setBallY] = useState(100);
   const [ballSpeedX, setBallSpeedX] = useState(5);
   const [ballSpeedY, setBallSpeedY] = useState(5);
-  const [ballDropped, setBallDropped] = useState(false); // Track if the ball has dropped below the paddle
+  const [lives, setLives] = useState(3);
+  const [score, setScore] = useState(0);
+  const [bricks, setBricks] = useState([
+    { id: 1, x: 50, y: 50, width: 60, height: 20, color: 'red' },
+    { id: 2, x: 150, y: 50, width: 60, height: 20, color: 'blue' },
+    // Add more bricks here
+  ]);
 
   useEffect(() => {
-    const handleKeyDown = (e) => {
-      if (e.code === 'ArrowLeft') {
-        setPaddleX((prevX) => Math.max(0, prevX - 10));
-      } else if (e.code === 'ArrowRight') {
-        setPaddleX((prevX) => Math.min(400, prevX + 10));
+    // Function to check collision between two rectangles
+    const isColliding = (rect1, rect2) => {
+      return (
+        rect1.x < rect2.x + rect2.width &&
+        rect1.x + rect1.width > rect2.x &&
+        rect1.y < rect2.y + rect2.height &&
+        rect1.y + rect1.height > rect2.y
+      );
+    };
+  
+    // Function to remove a brick from the array
+    const removeBrick = (brickId) => {
+      setBricks((prevBricks) => prevBricks.filter((brick) => brick.id !== brickId));
+      setScore((prevScore) => prevScore + 10); // Increase score
+    };
+  
+    // Check for brick collisions and remove bricks
+    bricks.forEach((brick) => {
+      const brickRect = {
+        x: brick.x,
+        y: brick.y,
+        width: brick.width,
+        height: brick.height,
+      };
+  
+      const ballRect = {
+        x: ballX,
+        y: ballY,
+        width: 10, // Ball's width
+        height: 10, // Ball's height
+      };
+  
+      if (isColliding(ballRect, brickRect)) {
+        removeBrick(brick.id);
+        setBallSpeedY((prevSpeedY) => -prevSpeedY); // Bounce the ball
       }
-    };
-
-    const updateBallPosition = () => {
-      if (!ballDropped) {
-        // If the ball has not dropped, update its position based on speed
-        setBallX((prevX) => prevX + ballSpeedX);
-        setBallY((prevY) => prevY + ballSpeedY);
-
-        // Check for boundary collisions
-        if (ballX < 0 || ballX > 480) {
-          setBallSpeedX((prevSpeedX) => -prevSpeedX);
-        }
-
-        // If the ball reaches the bottom, bounce it back up
-        if (ballY > 640) {
-          setBallSpeedY((prevSpeedY) => -prevSpeedY);
-          setBallDropped(true);
-        }
-
-        // Check for paddle collision
-        if (
-          ballY + 10 >= 600 && // Ball is at or below the top of the paddle
-          ballY + 10 <= 610 && // Ball is at or above the bottom of the paddle
-          ballX + 10 >= paddleX && // Ball is to the right of the left edge of the paddle
-          ballX <= paddleX + 80 // Ball is to the left of the right edge of the paddle
-        ) {
-          setBallSpeedY((prevSpeedY) => -prevSpeedY); // Bounce the ball back up
-        }
-      } else {
-        // If the ball has dropped below the paddle, reset it to the top
-        setBallX(240);
-        setBallY(0);
-        setBallSpeedY(5);
-        setBallDropped(false);
-      }
-    };
-
-    const gameLoop = setInterval(updateBallPosition, 20);
-
-    document.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      clearInterval(gameLoop);
-      document.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [ballX, ballY, ballSpeedX, ballSpeedY, ballDropped, paddleX]);
-
-  const bricksData = [
-    { id: 1, width: 60, height: 20, color: 'red' },
-    { id: 2, width: 60, height: 20, color: 'blue' },
-  ];
+    });
+  
+    // Check for game over condition
+    if (ballY > 640 && lives > 1) {
+      // Reset the ball and lose a life
+      setBallX(240);
+      setBallY(100);
+      setBallSpeedY(5);
+      setLives((prevLives) => prevLives - 1);
+    } else if (lives === 1 && ballY > 640) {
+      // Game over, no lives left
+      alert('Game Over! Your Score: ' + score);
+      // Implement more actions like restarting the game here if needed
+    }
+  
+    // Check for victory condition
+    if (bricks.length === 0) {
+      alert('Congratulations! You won!');
+      // Implement actions for winning the game, such as restarting or advancing to the next level
+    }
+  
+    // Update ball and paddle positions
+    setBallX((prevX) => prevX + ballSpeedX);
+    setBallY((prevY) => prevY + ballSpeedY);
+  
+    // Implement game restart
+    // You can add a function to restart the game here
+  
+  }, [ballX, ballY, ballSpeedX, ballSpeedY, paddleX, bricks, lives, score]);
+    
+  // Render bricks
 
   return (
     <div>
       <h1>Breakout Game</h1>
       <GameContainer>
         <Paddle x={paddleX} y={600} width={80} height={10} />
-        {bricksData.map((brick) => (
+        {bricks.map((brick) => (
           <Brick key={brick.id} width={brick.width} height={brick.height} color={brick.color} />
         ))}
         <div
@@ -97,6 +113,10 @@ const App = () => {
           }}
         />
       </GameContainer>
+      <div>
+        <p>Lives: {lives}</p>
+        <p>Score: {score}</p>
+      </div>
     </div>
   );
 };
